23e5f46f03c45ba36319d28e99d091e43b0df963
tiwaritech18 <rt82736736@gmail.com>
2024-05-02 21:23:53 +0530

    updated

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -8,7 +8,7 @@ const Category = ({ imgSrc, title}) => {
   
   return (
       <div className="category-banner">
-        <Img src={imgSrc} className='category-img' />
+        <LazyLoadImage src={imgSrc} className='category-img' />
         <div className="category-button">
             <h1>{title}</h1>
             <button className="button detail-btn">
